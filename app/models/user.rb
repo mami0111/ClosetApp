e79ffefe0c5116fe8d555bce8b9a579dca4efd1a
mami0111 <winter11sheep@gmail.com>
2018-07-07 12:21:40 +0900

    Userモデルにnameカラム追加。sign_upページに表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
- 
+  # 自分で追加したnameカラムにバリデーション（ミニュ力はエラー）を追加
+  validates :name, presence:true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
